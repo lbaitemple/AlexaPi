e594592fb29987d4fa7c93b88ac1bc2d2d40b4ce
Lenny Shirley <ircmaster@hotmail.com>
2016-03-22 14:50:05 -0400

    Update setup.sh
    Removed mpg123 from install list

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 wget --output-document vlc.py "http://git.videolan.org/?p=vlc/bindings/python.git;a=blob_plain;f=generated/vlc.py;hb=HEAD"
 apt-get update
-apt-get install libasound2-dev memcached python-pip mpg123 python-alsaaudio vlc -y
+apt-get install libasound2-dev memcached python-pip python-alsaaudio vlc -y
 pip install -r requirements.txt
 cp initd_alexa.sh /etc/init.d/AlexaPi
 update-rc.d AlexaPi defaults
